256213d9dbd257dfb81f53899a3e16020e42742d
Robby Lewis <robbyklew@gmail.com>
2020-03-20 08:49:27 -0700

    updated files

--- index.js
+++ index.js
@@ -5,30 +5,6 @@ $('#toggle').click(function() {
    });
 
 
-   console.log('clicked')
 
 
 
-//    let seconds = 5000;
-
-//    function change (){
-//     $("html, body").css('background-color','#a2c4e5');
-//    setTimeout(change1, seconds)
-//   }
-  
-//   function change1 (){
-//     $("html").css('background-color','#3c3c3c');
-//    setTimeout(change2, seconds)
-//   }
-    
-//   function change2 (){
-//     $("html").css('background-color','#4a2f15');
-//     setTimeout(change3, seconds)
-//    }
-    
-//   function change3 (){
-//     $("html").css('background-color','#1c375b');
-//    setTimeout(change, seconds)
-//   }
-  
-//   change();
